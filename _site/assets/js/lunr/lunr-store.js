861902df2f2c79537d725a6dbf338389c168713a
vogella <Lars.Vogel@vogella.com>
2023-01-24 11:01:00 +0000

    Update build to output generated at bfd9bb0

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Die Forscher des Kaisers gegen den Orden der Sieben",
+        "excerpt":"Kaum zu glauben: Am 18. September waren wir knapp 50 Spieler und NSC, die auch zwei heftigen Regenschauern getrotzt haben. Eigentlich selbstverständlich, oder habt Ihr schonmal gelesen, dass Legolas gesagt hat: „Elben, Zwerge, Menschen! Die Schlacht gegen Sauron muss heute leider ausfallen – denn es regnet.“ Schließlich ging es in...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/forscher_gegen_orden_der_sieben/",
+        "teaser": "/assets/images/homepage/kampf.jpeg"
+      },{
+        "title": "  Halloween-Sonderaktion Kämpfen, Klönen kleiner Flohmarkt",
+        "excerpt":"Damit die Wartezeit bis zum nächsten Baradur-LARP am 12. November nicht zu lang wird: Am Tag vor Halloween gibt es einen kleinen LARP-Flohmarkt. Die Spieler haben auch die Möglichkeit ein bisschen zu kämpfen, sich auszutauschen – und ggf. selber Artikel anzubieten und zu tauschen. Man muss ja nicht alles teuer...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/halloween/",
+        "teaser": "/assets/images/header.jpg"
+      },{
+        "title": "Auf ein larpiges 2023",
+        "excerpt":"Liebe LARP-Spieler, liebe Eltern, hier bekommt Ihr schon die Termine für 2023 (basteln und LARP) – mit der Chance auf Frühbucher-Rabatt: -Sonntag, 15. Januar 12.00-16.00 Uhr Uhr basteln (die Halle zum Kämpfen steht auch zur Verfügung) im Haus der Jugend Niendorf, Vielohweg 152, 22455 Hamburg (Nähe U2 Niendorf-Nord und Bus)...","categories": ["events"],
+        "tags": [],
+        "url": "/events/frohes-neues/",
+        "teaser": "/assets/images/homepage/dorf.jpeg"
+      },{
+        "title": "Bastel workshop 15.01.2023",
+        "excerpt":"Am 15. Januar sind wir zusammengekommen, um zu basteln. Wir hatten ca. 25 Besucher und haben mit ihnen gebastelt, gekämpft, und gegessen ;). Wir haben Schwerthalter, Fellumhänge, Arm/Beinschienen, Ketten und Geldbeutel gemacht. Bilder dazu seht Ihr unten. Wir hoffen viel von der Ausrüstung und Kleidung beim nächsten LARP zu sehen....","categories": ["events"],
+        "tags": [],
+        "url": "/events/bastellarp/",
+        "teaser": "/assets/images/header.jpg"
+      }]
